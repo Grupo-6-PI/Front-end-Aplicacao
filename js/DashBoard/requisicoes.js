4b85c07fab41a852016bd7a91ac5346d3de3b7f6
DaviRdaSilva <davi.rsilva@sptech.school>
2024-06-18 23:43:47 -0300

    feat/Integração com O grafico feita

--- js/DashBoard/requisicoes.js
+++ js/DashBoard/requisicoes.js
@@ -1,106 +1,123 @@
+async function buscarDados(){
 
-
-const data = {
-    labels: ['T1', 'T2', 'T3', 'T4'],
-    datasets: [
-        {
-            label: 'Cestas - Requeridas',
-            data: [50, 70, 80, 90],
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            stack: 'Cestas'
-        },
-        {
-            label: 'Cestas - Cumpridas',
-            data: [45, 60, 75, 85],
-            backgroundColor: 'rgba(255, 99, 132, 1)',
-            stack: 'Cestas'
-        },
-        {
-            label: 'Vestuário - Requeridas',
-            data: [30, 40, 50, 60],
-            backgroundColor: 'rgba(54, 162, 235, 0.5)',
-            stack: 'Vestuário'
-        },
-        {
-            label: 'Vestuário - Cumpridas',
-            data: [25, 35, 45, 55],
-            backgroundColor: 'rgba(54, 162, 235, 1)',
-            stack: 'Vestuário'
-        },
-        {
-            label: 'Saúde e Bem-estar - Requeridas',
-            data: [20, 30, 40, 50],
-            backgroundColor: 'rgba(75, 192, 192, 0.5)',
-            stack: 'Saúde e Bem-estar'
-        },
-        {
-            label: 'Saúde e Bem-estar - Cumpridas',
-            data: [15, 25, 35, 45],
-            backgroundColor: 'rgba(75, 192, 192, 1)',
-            stack: 'Saúde e Bem-estar'
-        },
-        {
-            label: 'Outros - Requeridas',
-            data: [10, 20, 30, 40],
-            backgroundColor: 'rgba(153, 102, 255, 0.5)',
-            stack: 'Outros'
-        },
-        {
-            label: 'Outros - Cumpridas',
-            data: [8, 12, 28, 38],
-            backgroundColor: 'rgba(153, 102, 255, 1)',
-            stack: 'Outros'
+    let retorno = await fetch(`http://localhost:8080/requisicoes/dash_requisicao`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
         }
-    ]
-};
+    })
 
-// Configuração do gráfico
-const config = {
-    type: 'bar',
-    data: data,
-    options: {
-        plugins: {
-            title: {
-                display: true,
-                text: 'Quantidade de Doações por Trimestre'
-            },
-            tooltip: {
-                mode: 'index',
-                intersect: true
-            },
-            legend: {
-                display: false
-            },
-            responsive: true
-        },
-        scales: {
-            xAxes: [{
-                stacked: false
-            }],
-            yAxes: [{
-                stacked: true
-            }],
-            x: {
-                stacked: true,
-                categoryPercentage: 1.0,
-                barPercentage: 0.9 // Ajuste o espaçamento entre as barras
+    if(retorno.status == 200){
 
-            },
-            y: {
-                stacked: true,
-                title: {
-                    display: true,
-                    text: 'Quantidade de Doações'
+        let dados = await retorno.json()
 
+        const data = {
+            labels: ['T1', 'T2', 'T3', 'T4'],
+            datasets: [
+                {
+                    label: 'Cestas - Cumpridas',
+                    data: dados.cesta_cum,
+                    backgroundColor: 'rgba(255, 99, 132, 1)',
+                    stack: 'Cestas'
+                },
+                {
+                    label: 'Cestas - Requeridas',
+                    data: dados.cesta_req,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                    stack: 'Cestas'
+                },
+                {
+                    label: 'Vestuário - Cumpridas',
+                    data: dados.vestuario_cum,
+                    backgroundColor: 'rgba(54, 162, 235, 1)',
+                    stack: 'Vestuário'
+                },
+                {
+                    label: 'Vestuário - Requeridas',
+                    data: dados.vestuario_req,
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                    stack: 'Vestuário'
+                },
+                {
+                    label: 'Saúde e Bem-estar - Cumpridas',
+                    data: dados.saude_cum,
+                    backgroundColor: 'rgba(75, 192, 192, 1)',
+                    stack: 'Saúde e Bem-estar'
+                },
+                {
+                    label: 'Saúde e Bem-estar - Requeridas',
+                    data: dados.saude_req,
+                    backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                    stack: 'Saúde e Bem-estar'
+                },
+                {
+                    label: 'Outros - Cumpridas',
+                    data: dados.outro_cum,
+                    backgroundColor: 'rgba(153, 102, 255, 1)',
+                    stack: 'Outros'
+                },
+                {
+                    label: 'Outros - Requeridas',
+                    data: dados.outro_req,
+                    backgroundColor: 'rgba(153, 102, 255, 0.5)',
+                    stack: 'Outros'
+                }
+            ]
+        };
+        
+        // Configuração do gráfico
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Quantidade de Doações por Trimestre'
+                    },
+                    tooltip: {
+                        mode: 'index',
+                        intersect: true
+                    },
+                    legend: {
+                        display: false
+                    },
+                    responsive: true
+                },
+                scales: {
+                    xAxes: [{
+                        stacked: false
+                    }],
+                    yAxes: [{
+                        stacked: true
+                    }],
+                    x: {
+                        stacked: true,
+                        categoryPercentage: 1.0,
+                        barPercentage: 0.9 // Ajuste o espaçamento entre as barras
+                    },
+                    y: {
+                        stacked: true,
+                        title: {
+                            display: true,
+                            text: 'Quantidade de Doações'
+        
+                        }
+                    }
                 }
             }
-        }
-    }
-};
+        };
+        
+        // renderizando o gráfico
+        const stackedBarChart = new Chart(
+            document.getElementById('stackedBarChart'),
+            config
+        );
 
-// renderizando o gráfico
-const stackedBarChart = new Chart(
-    document.getElementById('stackedBarChart'),
-    config
-);
+    }else{
+        console.log(retorno.status)
+        console.log(retorno.statusText)
+    }
+
+}
 
