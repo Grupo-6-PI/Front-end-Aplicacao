0f7b281140af26416104b2fa19c8a2950f3bb7bd
DaviRdaSilva <davi.rsilva@sptech.school>
2024-09-21 19:29:08 -0300

    Feat/Listagem de pedidos na pagina do beneficiado

--- public/js/pedido.js
+++ public/js/pedido.js
@@ -30,12 +30,84 @@ async function listar_tipo(){
 
 async function salvar_requisicao(){
 
-    var modal = document.getElementById("myModal");
+    var modalAcerto = document.getElementById("myModal");
+    var modalErro = document.getElementById("myModal");
     
-    if(true){
-        modal.style.display = "flex"
-    }else{
-        modal.style.display = "block"
+    var assunto = parseInt(document.getElementById("categorias-pedido").value); 
+    var descricao = document.getElementById("mensagem-pedido").value;
+
+    await axios.post(`${window.BASE_URL}/requisicoes`,{
+        "assuntoRequisicaoId":assunto,
+        "descricao":descricao
+    }).then((response) => {
+
+        if(response.status == 201){
+            modalAcerto.style.display = "flex"
+        }
+
+    })
+    .catch((error) => {
+
+        modalErro.style.display = "flex"
+
+        console.log(`
+        Status erro : ${error.status}
+        Mensagem erro : ${error.statusText}    
+        `)
+
+    })
+
+}
+
+async function listar_pedidos(){
+
+    var cards = document.getElementById('cards');
+    cards.innerHTML=""
+
+    var requisicao = await axios(`${window.BASE_URL}/?`,{
+        headers: {
+            'ngrok-skip-browser-warning': 'true'
+        }
+    })
+
+    if(requisicao.status == 200){
+
+        var response = await requisicao.data
+
+        response.map((pedido) => {
+
+            if(pedido.status == "Pronto"){
+
+                cards.innerHTML+=`
+                    <div class="card">
+                        <h4>PEDIDO #${pedido.id}</h4>
+                        <h3>${pedido.assunto}</h3>
+                        <p>"${pedido.descricao}"</p>
+                        <p id="footer-card"><strong>Status:</strong> <span class="status pronto">Pronto</span> <img src="./assets/imgs/aceito.png" alt=""></i></p>
+                    </div>
+                `
+            }else if(pedido.assunto == "Em andamento"){
+                cards.innerHTML+=`
+                    <div class="card">
+                        <h4>PEDIDO #${pedido.id}</h4>
+                        <h3>${pedido.assunto}</h3>
+                        <p>"${pedido.descricao}"</p>
+                        <p id="footer-card"><strong>Status:</strong> <span class="status negado">Negado</span> <img src="./assets/imgs/negado.png" alt=""></i></p>
+                    </div>
+                `
+            }else{
+                cards.innerHTML+=`
+                    <div class="card">
+                        <h4>PEDIDO #${pedido.id}</h4>
+                        <h3>${pedido.assunto}</h3>
+                        <p>"${pedido.descricao}"</p>
+                        <p id="footer-card"><strong>Status:</strong> <span class="status andamento">Produzindo</span></i></p>
+                    </div>
+                `
+            }
+
+        })
+
     }
 
 }
