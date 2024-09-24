a83174cd971b3d82872fad2fe2c6abb6dd29b0f2
Zuull21 <julio.dahi@sptech.school>
2024-09-24 20:40:17 -0300

    feat/Integração Financeiro Listagem de Categorias no JS

---
+++ public/js/DashBoard/financeiro.js
@@ -0,0 +1,29 @@
+
+async function listar_categoria(){
+
+    var lista = document.getElementById('container-select-categoria')
+    lista.innerHTML=""
+
+    try {
+
+        var requisicao = await axios(`${window.BASE_URL}/vendas/listar-categorias`,{
+            headers: {
+                'ngrok-skip-browser-warning': 'true'
+            }
+        })
+
+        var data = await requisicao.data
+
+        data.map((tipo) => {
+
+            lista.innerHTML+=`
+                <option value="${tipo.id}">${tipo.nome}</option>
+            `
+
+        })
+
+    }catch (error) {
+        console.log(error)
+    }
+
+}
