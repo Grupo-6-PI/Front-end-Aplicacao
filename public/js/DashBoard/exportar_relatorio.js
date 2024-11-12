f21b5adf91cee2f8625bbd28ce661c4941376f6d
Zuull21 <julio.dahi@sptech.school>
2024-11-12 20:58:44 -0300

    refactor/nome do arquivo csv com data

--- public/js/DashBoard/exportar_relatorio.js
+++ public/js/DashBoard/exportar_relatorio.js
@@ -110,7 +110,7 @@ async function exportarRelatorio(tipo) {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', `relatorio_financeiro.${tipo}`);
+        link.setAttribute('download', `relatorio_financeiro${dataInicio}_${dataFim}.${tipo}`);
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
