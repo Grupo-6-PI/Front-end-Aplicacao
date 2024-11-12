c550772dd14ebc6659ec8e73c26f6f309b626a97
Zuull21 <julio.dahi@sptech.school>
2024-11-12 20:52:15 -0300

    refactor/alteração de caminho de exportação

--- public/js/DashBoard/exportar_relatorio.js
+++ public/js/DashBoard/exportar_relatorio.js
@@ -82,3 +82,40 @@ const dados = [
 
 // Adiciona a função ao escopo global
 window.gerarRelatorioPDF = gerarRelatorioPDF;
+
+async function exportarRelatorio(tipo) {
+    const dataInicio = document.getElementById('datetime1').value;
+    const dataFim = document.getElementById('datetime2').value;
+    // const categoria = document.getElementById('categoria').value;
+    const emailUsuario = sessionStorage.getItem('EMAIL_USER');
+
+    if (!dataInicio || !dataFim) {
+        alert("Por favor, selecione as datas de início e fim.");
+        return;
+    }
+
+    try {
+        const response = await axios.get(`${window.BASE_URL}/vendas/exportar`, {
+            params: {
+                dataInicio,
+                dataFim,
+                // categoria,
+                emailUsuario,
+                tipo
+            },
+            responseType: 'blob',
+            headers: { 'ngrok-skip-browser-warning': 'true' }
+        });
+
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `relatorio_financeiro.${tipo}`);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    } catch (error) {
+        console.error("Erro ao exportar relatório:", error);
+        alert("Erro ao exportar o relatório. Tente novamente.");
+    }
+}
